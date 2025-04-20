b21cd304fafd0b8b67dbf04095dc866c37d74e0b
spadaritucks <thiago.spadari02@gmail.com>
2025-04-19 23:55:57 -0300

    Adiciona nova página inicial com layout e estilos, incluindo imagem de pré-visualização. Remove a antiga implementação da página inicial.

---
+++ src/pages/home/styles.ts
@@ -0,0 +1,40 @@
+import { Heading, styled, Text } from "@ignite-ui/react";
+
+
+export const Container = styled('div', {
+    marginLeft: "auto",
+    display: 'flex',
+    alignItems: "center",
+    gap: "$20",
+    height: '100vh',
+    width: 1200,
+    maxWidth: "calc(100vw - (100vw - 1160px ) / 2)",
+    overflow: 'hidden'
+
+})
+
+export const Hero = styled('div', {
+    maxWidth: 480,
+    padding: '0 $10',
+
+    [`${Heading}`]: {
+        "@media(max-width : 600px)": {
+            fontSize : "$6xl"
+        }
+
+    },
+
+    [`${Text}`]: {
+        marginTop: '$2',
+        color: "$gray200",
+        fontSize: "$xl"
+    },
+})
+
+export const Preview = styled('div', {
+    paddingRight: "$8",
+    overflow: "hidden",
+    "@media(max-width : 600px)": {
+        display: "none"
+    }
+})
