6460f0591190145ad1f29010402d7775afc4a6ec
spadaritucks <thiago.spadari02@gmail.com>
2025-04-24 17:18:24 -0300

    Adiciona a biblioteca @tanstack/react-query ao projeto e implementa o gerenciamento de estado de consultas no componente CalendarStep, substituindo a lógica anterior de uso do useEffect. Atualiza o componente App para incluir o QueryClientProvider, garantindo que as consultas sejam gerenciadas corretamente.

--- src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -4,6 +4,8 @@ import { Container, TimePicker, TimePickerHeader, TimePickerItem, TimePickerList
 import dayjs from "dayjs";
 import { api } from "../../../../../lib/axios";
 import { useRouter } from "next/router";
+import { useQuery } from "@tanstack/react-query";
+
 
 interface Availability {
     possibleTimes: number[]
@@ -13,7 +15,7 @@ interface Availability {
 export function CalendarStep() {
 
     const [selectedDate, setSelectedDate] = useState<Date | null>(null)
-    const [availability, setAvailability] = useState<Availability | null>(null)
+
 
     const router = useRouter()
     const username = String(router.query.username)
@@ -24,17 +26,33 @@ export function CalendarStep() {
     const weekDay = selectedDate ? dayjs(selectedDate).format('dddd') : null
     const describleDate = selectedDate ? dayjs(selectedDate).format('DD[ de ] MMMM') : null
 
-    useEffect(() => {
-        if (!selectedDate) {
-            return
-        }
+    const selectedDateWithoutTime = selectedDate ? dayjs(selectedDate).format('YYYY-MM-DD') : null
 
-        api.get(`/users/${username}/availability`, {
-            params: {
-                date: dayjs(selectedDate).format('YYYY-MM-DD')
-            }
-        }).then(response => { setAvailability(response.data) })
-    }, [selectedDate])
+    const { data: availability } = useQuery<Availability>({
+        queryKey : ['availability', selectedDateWithoutTime],
+        queryFn : async () => {
+            const response = await api.get(`/users/${username}/availability`, {
+              params: {
+                date: selectedDateWithoutTime,
+              },
+            })
+      
+            return response.data
+          },
+          enabled: !!selectedDate
+    })
+
+    // useEffect(() => {
+    //     if (!selectedDate) {
+    //         return
+    //     }
+
+    //     api.get(`/users/${username}/availability`, {
+    //         params: {
+    //             date: 
+    //         }
+    //     }).then(response => { setAvailability(response.data) })
+    // }, [selectedDate])
 
     return (
         <Container isTimePickerOpen={isDateSelected}>
